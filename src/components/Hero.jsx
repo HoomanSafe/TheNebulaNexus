c94f1cd868230c4b755a0fd0edd59fdc666af604
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-08 19:17:47 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/img1.svg";
+import logo from "../assets/banner1.jpeg";
 
 function Hero() {
   return (
@@ -7,15 +7,15 @@ function Hero() {
       <div className="container grid lg:grid-cols-2 justify-between items-center gap-20">
         <article className="flex flex-col gap-6">
           <h1 className=" text-white text-5xl font-bold font-SpaceGro max-w-[500px] leading-[1.2]">
-            Hi, I’m Beast! mark Zuckerbergs puppy.{" "}
+            What is the Nexus Nebula?{" "}
           </h1>
           <p className="max-w-[522px] text-white text-base font-normal font-['Space Grotesk'] leading-loose tracking-tight">
-            I’m a Puli breed living in Palo Alto and have 2.4 million followers
-            on Facebook. I was adopted in 2011 by the tech giant Mark Zuckerberg
-            and his wife Priscilla Chan.
+            The Nexus Nebula is Utility
+We aim to build a bot that will be detected by fake smart contracts and this is a space unique bot on the Binance network, it is already in development and an official announcement will come once it is complete and ready to be tested
+
           </p>
           <a
-            href="https://t.me/BeastTheDog"
+            href="https://t.me/NebulaNexusBNB"
             className="w-[181px] block h-[52px] relative"
           >
             <div className="w-[173px] h-11 left-[8px] top-[8px] absolute bg-yellow-400" />
